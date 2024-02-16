88d5875dd5887e7386c487fc2cd7c3ab9422b38c
Amination-Gogia <gogia.aminesh@gmail.com>
2024-02-16 12:33:48 +0530

    Changing the word 'price' to 'cost'

--- my-app/src/cartItem.js
+++ my-app/src/cartItem.js
@@ -1,11 +1,11 @@
 // Assuming a food-item is represented as an object of the form:
-// foodItem = {name, price, .... other keys allowed but only these two are relavent)}   
+// foodItem = {name, cost, .... other keys allowed but only these two are relavent)}   
 
 function CartItem({foodItem, add, subtract, number}){
     return (
         <div className="food-item">
             <h3 className="name">{foodItem.name}</h3>
-            <h3 className="price">&#8377; {foodItem.price}</h3>
+            <h3 className="cost">&#8377; {foodItem.cost}</h3>
             <span className="quantity">
                 <button className="minus" onClick = {subtract}>-</button>
                 <button className="label" disabled>{number}</button>
