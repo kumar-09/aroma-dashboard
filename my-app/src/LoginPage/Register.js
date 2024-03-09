351c382db899ff57ccca78990cc35e25495a0bf9
thechosen2 <pravmalpani@gmail.com>
2024-03-09 18:58:11 +0530

    added registering functionality

--- my-app/src/LoginPage/Register.js
+++ my-app/src/LoginPage/Register.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = (props) => {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
@@ -9,7 +11,7 @@ const Register = (props) => {
 
   const navigate = useNavigate();
 
-  const onButtonClick = () => {
+  const onButtonClick = async() => {
     setEmailError('');
     setPasswordError('');
   
@@ -31,6 +33,24 @@ const Register = (props) => {
       setPasswordError('The password must be 8 characters or longer');
       return;
     }
+
+    let axiosConfig = {
+      headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+          "Access-Control-Allow-Origin": "*",
+      }
+    };
+    // let data = {
+    //   userid : email,
+    //   name : name,
+    //   password: password
+    // };
+    
+      axios({method : 'post', url: 'http://127.0.0.1:8000/api/register/', data : ({userid: email, name : name, pswd : password}), headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }})
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   }
 
   return (
@@ -39,11 +59,24 @@ const Register = (props) => {
         <div>Register</div>
       </div>
       <br />
+      <form>
       <div className={'inputContainer'}>
         <input
           value={email}
           placeholder="Enter your userID here"
           onChange={(ev) => setEmail(ev.target.value)}
+          type= "text"
+          className={'inputBox'}
+        />
+        <label className="errorLabel">{emailError}</label>
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input
+          value={name}
+          placeholder="Enter your name here"
+          onChange={(ev) => setName(ev.target.value)}
+          type= "text"
           className={'inputBox'}
         />
         <label className="errorLabel">{emailError}</label>
@@ -54,14 +87,17 @@ const Register = (props) => {
           value={password}
           placeholder="Enter your password here"
           onChange={(ev) => setPassword(ev.target.value)}
+          type = "password"
           className={'inputBox'}
         />
         <label className="errorLabel">{passwordError}</label>
       </div>
       <br />
       <div className={'inputContainer'}>
-        <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Register'} />
+        <input className={'inputButton'} type="submit" onClick={onButtonClick} value={'Register'} />
       </div>
+      </form>
+      
       <div className='noaccount'>
         Already have an account?
         <Link to="/login">Login Now!</Link>
