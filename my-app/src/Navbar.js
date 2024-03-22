d04ef8c38653249d47819af12394727206f05e70
thechosen2 <pravmalpani@gmail.com>
2024-03-23 00:42:34 +0530

    css changes to navbar components

--- my-app/src/Navbar.js
+++ my-app/src/Navbar.js
@@ -6,6 +6,7 @@ import catgimg from './image/category-svgrepo-com.svg'
 import cartimg from './image/cart-3-svgrepo-com.svg'
 import loginimg from './image/login-svgrepo-com.svg'
 import accountimg from './image/mine-svgrepo-com.svg'
+import adminimg from './image/admin-svgrepo-com.svg'
 const Navbar = (props) => {
     var NavbarRef = props.NavbarRef;
     var loggedIn = props.loggedIn;
@@ -36,10 +37,10 @@ const Navbar = (props) => {
                {!props.admin ? 
                <Link to='/Cart' className="nav-links"><img src={cartimg} alt={''} style={{width: '25px'}}></img>Cart
                </Link>:
-               <Link to='/admin' className="nav-links">Admin</Link>}
+               <Link to='/admin' className="nav-links" ><img src={adminimg} alt={''} style={{width: '25px', opacity: '0.6',}}></img>Admin</Link>}
                {loggedIn ? 
                <div style={{display:"flex", flexDirection:"column"}} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                    <Link className="nav-links" to='/account' id = "acc" ><img src={accountimg} alt={""} style={{width: '25px'}}/>{name}</Link>
+                    <Link className="nav-links" to='/account' id = "acc" style={{padding:'5px'}}><img src={accountimg} alt={""} style={{width: '27px', opacity: '0.6', padding:'0px'}}/>{name}</Link>
                 </div> : <Link to='/Login' className="nav-links" ><img src={loginimg} alt={""} style={{width: '25px'}}></img>Login</Link>}
             </nav>
         </div>
